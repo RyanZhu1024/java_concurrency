cba88b82ddffe69ab52c64fa341b9e4184a55e47
RyanZhu <troyzx1024@gmail.com>
2015-01-22 17:28:40 +0800

    change to use ReentrantReadWriteLock
    Signed-off-by: RyanZhu <troyzx1024@gmail.com>

--- src/main/java/producerconsumers/Buffer.java
+++ src/main/java/producerconsumers/Buffer.java
@@ -2,7 +2,7 @@ package producerconsumers;
 
 import java.util.LinkedList;
 import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Created by shuxuan on 1/22/15.
@@ -10,7 +10,7 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Buffer {
     private LinkedList<String> buffer;
     private int maxSize;
-    private ReentrantLock lock;
+    private ReentrantReadWriteLock lock;
     private Condition space;
     private Condition lines;
 
@@ -19,48 +19,41 @@ public class Buffer {
     public Buffer(int maxSize) {
         this.maxSize = maxSize;
         this.buffer = new LinkedList<String>();
-        this.lock = new ReentrantLock();
-        this.lines = lock.newCondition();
-        this.space = lock.newCondition();
+        this.lock = new ReentrantReadWriteLock();
+        this.space = lock.writeLock().newCondition();
         this.pendingLines = true;
     }
 
     public void insert(String line) {
-        this.lock.lock();
+        this.lock.writeLock().lock();
         try {
-            while (buffer.size() == maxSize) {
-                space.await();
-            }
             buffer.offer(line);
             System.out.printf("Thread: %s insert a line: size is: %d\n", Thread.currentThread().getName(), line.length());
-            this.lines.signalAll();
-        } catch (InterruptedException ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
         } finally {
-            this.lock.unlock();
+            this.lock.writeLock().unlock();
         }
     }
 
     public String get() {
         String line = null;
-        this.lock.lock();
+        this.lock.readLock().lock();
         try {
-            while (this.buffer.size() == 0 && hasPendingLines()) {
-                this.lines.await();
-            }
             if (hasPendingLines()) {
+                this.lock.readLock().unlock();
+                this.lock.writeLock().lock();
                 line = buffer.poll();
-//                if(line==null){
-//                    System.out.printf("Thread: %s read line: null\n", Thread.currentThread().getName());
-//                }else {
-                System.out.printf("Thread: %s read line: %d\n", Thread.currentThread().getName(), line.length());
-//                }
-                this.space.signalAll();
+                this.lock.writeLock().unlock();
+                this.lock.readLock().lock();
+                if(line!=null) {
+                    System.out.printf("Thread: %s read line: %d\n", Thread.currentThread().getName(), line.length());
+                }
             }
-        } catch (InterruptedException ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
         } finally {
-            this.lock.unlock();
+            this.lock.readLock().unlock();
         }
         return line;
     }
--- src/main/java/producerconsumers/Consumer.java
+++ src/main/java/producerconsumers/Consumer.java
@@ -1,12 +1,17 @@
 package producerconsumers;
 
 import java.util.Random;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by shuxuan on 1/22/15.
  */
 public class Consumer implements Runnable {
     private Buffer buffer;
+    private static AtomicInteger atomicInteger = new AtomicInteger();
+    private static ConcurrentSkipListSet<String> listSet = new ConcurrentSkipListSet<String>();
 
     public Consumer(Buffer buffer) {
         this.buffer = buffer;
@@ -21,8 +26,12 @@ public class Consumer implements Runnable {
     }
 
     private void processLine(String line) {
-
-        System.out.println("Right now processing the line: " + line);
+        if (line != null) {
+            System.out.println("Right now processing the line: " + line);
+            System.out.println(atomicInteger.incrementAndGet());
+            listSet.add(line);
+            System.out.println("All elements got from the File Mock currently: " + listSet.size());
+        }
         try {
             Random random = new Random();
             Thread.sleep(random.nextInt(32));
