c60c4ecffd53a9279d61d1dff751a387e9196760
RyanZhu <troyzx1024@gmail.com>
2015-01-29 17:15:21 +0800

    for join init
    Signed-off-by: RyanZhu <troyzx1024@gmail.com>

--- src/main/java/forkjoin/Main1.java
+++ src/main/java/forkjoin/Main1.java
@@ -1,7 +1,43 @@
 package forkjoin;
 
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by shuxuan on 1/29/15.
  */
 public class Main1 {
+    public static void main(String[] args) {
+        ProductGenerator productGenerator = new ProductGenerator();
+        List<Product> products = productGenerator.generate(10000);
+        Task task = new Task(products, 0, products.size(), 0.20);
+        ForkJoinPool pool = new ForkJoinPool();
+        pool.execute(task);//asynchronous call
+        do {
+            System.out.printf("Main: Thread Count: %d\n", pool.
+                    getActiveThreadCount());
+            System.out.printf("Main: Thread Steal: %d\n", pool.
+                    getStealCount());
+            System.out.printf("Main: Parallelism: %d\n", pool.
+                    getParallelism());
+            try {
+                TimeUnit.MILLISECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } while (!task.isDone());
+        pool.shutdown();
+        if (task.isCompletedNormally()) {
+            System.out.printf("Main: The process has completed normally\n");
+        }
+
+        for (int i = 0; i < products.size(); i++) {
+            Product product = products.get(i);
+            if (product.getPrice() != 12) {
+                System.out.printf("Product %s: %f\n", product.
+                        getName(), product.getPrice());
+            }
+        }
+    }
 }
--- src/main/java/forkjoin/Task.java
+++ src/main/java/forkjoin/Task.java
@@ -1,7 +1,55 @@
 package forkjoin;
 
+import java.util.List;
+import java.util.concurrent.RecursiveAction;
+
 /**
  * Created by shuxuan on 1/29/15.
  */
-public class Task {
+public class Task extends RecursiveAction {
+    private static final long serialVersionUID = 1L;
+
+    private List<Product> products;
+    private int first;
+    private int last;
+    private double increment;
+
+    public Task(List<Product> products, int first, int last, double increment) {
+        this.products = products;
+        this.first = first;
+        this.last = last;
+        this.increment = increment;
+    }
+
+    @Override
+    protected void compute() {
+        if (last - first < 10) {
+            updatePrice();
+        } else {
+            int middle = (last + first) / 2;
+//            System.out.println("Pending tasks: " + getQueuedTaskCount());
+            System.out.println("Active thread count: "+Thread.activeCount());
+            Task t1 = new Task(products, first, middle + 1, increment);
+            Task t2 = new Task(products, middle + 1, last, increment);
+            if (inForkJoinPool()) {
+                invokeAll(t1, t2);//synchronous call, working thread will take other
+                                    // tasks to execute and create new threads if necessary
+                if(first==0&&last==10000) {
+                    /*
+                    current task will be suspended until all subtasks finished
+                     */
+                    System.out.println("in the forkjoinpool, two tasks for first: " + first +
+                            " and middle: " + middle + " and last: " + last);
+                    System.out.println("Current thread name: " + Thread.currentThread().getName());
+                }
+            }
+        }
+    }
+
+    private void updatePrice() {
+        for (int i = first; i < last; i++) {
+            Product product = products.get(i);
+            product.setPrice(product.getPrice() * (1 + increment));
+        }
+    }
 }
